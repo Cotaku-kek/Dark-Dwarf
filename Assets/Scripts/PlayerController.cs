4a49f04057984d390aca7fbbc2f261c2e6e1b839
Cristina <c.nichita@web.de>
2023-01-30 20:19:48 +0100

    Wall jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,34 +8,43 @@ public class PlayerController : MonoBehaviour
 {
     private float horizontal;
     private bool isFacingRight = true;
-    private float movementSpeed = 50f;
-    private float jumpHeight = 15f;
+
+    public float movementSpeed;             
+    public float jumpHeight;                
+    public float walljumpPower;
+    //public float walljumpPowerSide;          
+
+    public Animator animator;
+    private BoxCollider2D boxCollider;
     private Rigidbody2D rigidbody;
     private PlayerInput playerInput;
     private PlayerInputActions playerInputActions;
-    public Animator animator;
+    private float wallJumpCooldown;
 
-    [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
 
     void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
         playerInput = GetComponent<PlayerInput>();
+        boxCollider = GetComponent<BoxCollider2D>();
         GetPlayerInputActions();
     }
 
     void Update()
     {
-        Flip();
+        FlipImage();
         Movement();
+        WallJump();
         horizontal = Input.GetAxisRaw("Horizontal");
-        //animator.SetBool("isJumping", false);                                                     // temporaer
+        //animator.SetBool("isJumping", false);                                                         // temporaer
+
     }
 
     void GetPlayerInputActions()
     {
-        playerInputActions = new PlayerInputActions();                                              // Access to the PlayerInputActions script
+        playerInputActions = new PlayerInputActions();                                                  // Access to the PlayerInputActions script
         playerInputActions.Player.Enable();
         playerInputActions.Player.Jump.performed += Jump;
     }
@@ -50,10 +59,37 @@ public class PlayerController : MonoBehaviour
     {
         if(context.performed && IsGrounded())
         {
-            print("Jump! " + context.phase);                                                        // Check which phase is active (started, performed or canceled)
+            print("Jump! " + context.phase);                                                            // Check which phase is active (started, performed or canceled)
             rigidbody.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);                       
             animator.SetBool("isJumping", true);
         }
+        else if(context.performed && !IsGrounded() && OnWall())
+        {
+            rigidbody.velocity = new Vector2(-Mathf.Sign(transform.localScale.x), walljumpPower);       // * wallJumpPowerSide, 
+            wallJumpCooldown = 0;
+        }
+    }
+
+    void WallJump()
+    {
+        if(wallJumpCooldown > 0.2f)
+        {
+            if(OnWall() && !IsGrounded())
+            {
+                rigidbody.gravityScale = 0;
+                rigidbody.velocity = Vector2.zero;
+            }
+            else
+            {
+                rigidbody.gravityScale = 3;
+            }
+            playerInputActions.Player.Jump.performed += Jump;
+        }
+        else
+        {
+            wallJumpCooldown += Time.deltaTime;
+        }
+
     }
 
     private void FixedUpdate()
@@ -61,13 +97,19 @@ public class PlayerController : MonoBehaviour
         rigidbody.velocity = new Vector2(horizontal, rigidbody.velocity.y);                         // Control players speed    
     }
 
-    private bool IsGrounded()
+    private bool IsGrounded()                                                                       // Check if player is on ground
     {
-        // Invisible circle around player, which collides with ground
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
     }
 
-    private void Flip()
+    private bool OnWall()                                                                           // Check if player is on wall
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
+    }
+
+    private void FlipImage()
     {
         // Flip player image to left or right while moving
         if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
