f819f099497ebf4edf63d63e5509dfcdf5ecfdb9
Cristina <c.nichita@web.de>
2023-01-31 15:49:48 +0100

    Wall bouncing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,23 +6,33 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private float horizontal;
-    private bool isFacingRight = true;
-
-    public float movementSpeed;             
-    public float jumpHeight;                
-    public float walljumpPower;
-    //public float walljumpPowerSide;          
 
     public Animator animator;
     private BoxCollider2D boxCollider;
     private Rigidbody2D rigidbody;
     private PlayerInput playerInput;
     private PlayerInputActions playerInputActions;
-    private float wallJumpCooldown;
+
+    private float horizontal;
+    private bool isFacingRight = true;
+    private bool isWallSliding;
+
+    // Wall Logic
+    private bool isWallJumping;
+    private float wallJumpingCounter;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.8f;
+    private float wallJumpingDuration = 0.8f;
+    [SerializeField] Vector2 wallJumpingPower = new Vector2(4f, 1f);
+
+    [SerializeField] float movementSpeed;             
+    [SerializeField] float jumpHeight;                          
+    [SerializeField] float wallSlidingSpeed;
 
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform wallCheck;
 
     void Awake()
     {
@@ -34,12 +44,26 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        FlipImage();
-        Movement();
-        WallJump();
         horizontal = Input.GetAxisRaw("Horizontal");
+        Movement();
+        WallSlide();
+        WallJump();
+
+        if(!isWallJumping)
+        {
+            FlipImage();
+        }
+
         //animator.SetBool("isJumping", false);                                                         // temporaer
 
+    }
+
+    private void FixedUpdate()
+    {
+        if(!isWallJumping)
+        {
+            rigidbody.velocity = new Vector2(horizontal, rigidbody.velocity.y);                             // Control players speed    
+        }
     }
 
     void GetPlayerInputActions()
@@ -63,50 +87,70 @@ public class PlayerController : MonoBehaviour
             rigidbody.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);                       
             animator.SetBool("isJumping", true);
         }
-        else if(context.performed && !IsGrounded() && OnWall())
-        {
-            rigidbody.velocity = new Vector2(-Mathf.Sign(transform.localScale.x), walljumpPower);       // * wallJumpPowerSide, 
-            wallJumpCooldown = 0;
-        }
     }
 
     void WallJump()
     {
-        if(wallJumpCooldown > 0.2f)
+        if(isWallSliding)
         {
-            if(OnWall() && !IsGrounded())
-            {
-                rigidbody.gravityScale = 0;
-                rigidbody.velocity = Vector2.zero;
-            }
-            else
-            {
-                rigidbody.gravityScale = 3;
-            }
-            playerInputActions.Player.Jump.performed += Jump;
+            isWallJumping = false;
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+
+            CancelInvoke(nameof(StopWallJumping));
         }
         else
         {
-            wallJumpCooldown += Time.deltaTime;
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if(playerInputActions.Player.Jump.triggered && wallJumpingCounter > 0f)
+        {
+            playerInputActions.Player.Movement.Disable();
+            isWallJumping = true;
+            rigidbody.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+
+            if(transform.localScale.x != wallJumpingDirection)
+            {
+                isFacingRight = !isFacingRight;
+                Vector3 localScale = transform.localScale;
+                localScale.x *= -1f;
+                transform.localScale = localScale;
+            }
+
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
         }
 
     }
 
-    private void FixedUpdate()
+    void StopWallJumping()
     {
-        rigidbody.velocity = new Vector2(horizontal, rigidbody.velocity.y);                         // Control players speed    
+        playerInputActions.Player.Movement.Enable();        
+        isWallJumping = false;
     }
 
-    private bool IsGrounded()                                                                       // Check if player is on ground
+    void WallSlide()
     {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
-        return raycastHit.collider != null;
+        if(OnWall() && !IsGrounded() && horizontal != 0f)
+        {
+            isWallSliding = true;
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, Mathf.Clamp(rigidbody.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
     }
 
-    private bool OnWall()                                                                           // Check if player is on wall
+    private bool IsGrounded()                                                                           // Check if player is on ground
     {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
-        return raycastHit.collider != null;
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private bool OnWall()                                                                               // Check if player is on wall
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
     }
 
     private void FlipImage()
